a4b13ddc6ffdfa15bd6e419d5c920705886768d3
Oscar Bosigas <oscar7bosigas@gmail.com>
2024-10-19 16:02:50 -0500

    restore

--- front/about.py
+++
@@ -1,8 +0,0 @@
-import streamlit as st
-
-def app():
-    st.subheader('INVOICIFY es una aplicacion creada para ')
-    st.subheader('PYMES con el fin de automatizar y analizar')
-    st.subheader('todas sus facturas electronicas')
-    st.markdown('Creado por: Opeen Data')
-    st.markdown('Contacto: https://opeendata.com/')
